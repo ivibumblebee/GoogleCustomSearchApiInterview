9f994e37fbd430ce50784e3e1ff1cefeb08cf376
Patrique Legault <patrique.legault@uottawa.ca>
2018-08-01 12:03:57 -0400

    Adding the first part of the changes to the repo

--- my-app/src/test/java/com/mycompany/app/AppTest.java
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -1,38 +1,28 @@
 package com.mycompany.app;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.UnsupportedEncodingException;
+
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
+    @Test
+    public void testURL1() throws UnsupportedEncodingException
     {
-        super( testName );
+    	assertNotNull(App.getQuery("sample_input"));
+    }
+    
+    @Test
+    public void testURL2() throws UnsupportedEncodingException
+    {
+    	assertNotNull(App.getQuery("sample input"));
+    	assertEquals(App.getQuery("sample input"), "sample+input");
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
